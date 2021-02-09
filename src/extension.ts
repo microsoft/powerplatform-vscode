a5bf50e2ea14903b9ee54774b6db6ceaca3b4391
David JENNI <david@davidjenni.com>
2021-02-09 14:37:29 -0800

    update to latest pac CLI (#14)

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { CliAcquisition } from './lib/CliAcquisition';
 import { PacTerminal } from './lib/PacTerminal';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-    const cli = new CliAcquisition(context, '1.4.4');
+    const cli = new CliAcquisition(context, '1.5.2');
     const cliPath = await cli.ensureInstalled();
     context.subscriptions.push(cli);
     context.subscriptions.push(new PacTerminal(context, cliPath));
