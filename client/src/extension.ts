f0a8d261f00dadf120a0e625f18bbabf7ea720e8
Ashish Choudhary <aschoud@microsoft.com>
2021-04-08 22:13:16 +0530

    added fcb check for portal features

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -16,45 +16,12 @@ import {
 let client: LanguageClient;
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-    // The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
 
-    // Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-
-    // Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
-
-	// Start the client. This will also launch the server
-	client.start();
+    const isPaportalFeatureEnabled = vscode.workspace.getConfiguration('powerplatform-vscode').get('enablePortalFeatures');
+    if (isPaportalFeatureEnabled) {
+        // add  portal specific features in this block
+    }
 
     const cli = new CliAcquisition(context, '1.5.2');
     const cliPath = await cli.ensureInstalled();
