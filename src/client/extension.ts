7558a83927c781bb37220d05805e7a2ccf7d5532
Ashish Choudhary <ashish.vit06@gmail.com>
2021-05-20 20:54:05 +0530

    Users/aschoud/auto complete refactor (#33)
    * implemented autocomplete for portal configurations
    
    * modified the condition to detect manifest file for autocomplete
    
    * fixed all the lint issues
    
    * renamed language server
    
    * removed yml language from the configuration as VSCode recommends to use the known language identifier
    
    * fixed formatting of files
    
    * fixed PR comments
    
    * moved require statement to the top
    
    Co-authored-by: Ashish Choudhary <aschoud@microsoft.com>

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -19,53 +19,54 @@ let client: LanguageClient;
 export async function activate(
     context: vscode.ExtensionContext
 ): Promise<void> {
-    // The server is implemented in node
-    const serverModule = context.asAbsolutePath(
-        path.join("dist", "server.js")
-    );
-    // The debug options for the server
-    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: debugOptions,
-        },
-    };
-
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [{ scheme: "file", language: "plaintext" }],
-        synchronize: {
-            // Notify the server about file changes to '.clientrc files contained in the workspace
-            fileEvents: vscode.workspace.createFileSystemWatcher(
-                "**/.clientrc"
-            ),
-        },
-    };
-
-    // Create the language client and start the client.
-    client = new LanguageClient(
-        "languageServerExample",
-        "Language Server Example",
-        serverOptions,
-        clientOptions
-    );
-
-    // Start the client. This will also launch the server
-    client.start();
-
     const isPaportalFeatureEnabled = vscode.workspace
         .getConfiguration("powerplatform-vscode")
         .get("enablePortalFeatures");
     if (isPaportalFeatureEnabled) {
         // add  portal specific features in this block
+
+        // The server is implemented in node
+        const serverModule = context.asAbsolutePath(
+            path.join("dist", "server.js")
+        );
+        // The debug options for the server
+        // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+        const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+        // If the extension is launched in debug mode then the debug server options are used
+        // Otherwise the run options are used
+        const serverOptions: ServerOptions = {
+            run: { module: serverModule, transport: TransportKind.ipc },
+            debug: {
+                module: serverModule,
+                transport: TransportKind.ipc,
+                options: debugOptions,
+            },
+        };
+
+        // Options to control the language client
+        const clientOptions: LanguageClientOptions = {
+            // Register the server for plain text documents
+            documentSelector: [{ scheme: "file", language: "yaml" }],
+            synchronize: {
+                // Notify the server about file changes to '.clientrc files contained in the workspace
+                fileEvents: vscode.workspace.createFileSystemWatcher(
+                    "**/.clientrc"
+                ),
+            },
+        };
+
+        // Create the language client and start the client.
+        client = new LanguageClient(
+            "PowerappsLanguageServer",
+            "PowerApps Language Server",
+            serverOptions,
+            clientOptions
+        );
+
+        // Start the client. This will also launch the server
+        client.start();
+
         // portal web view panel
         context.subscriptions.push(
             vscode.commands.registerCommand(
@@ -84,7 +85,7 @@ export async function activate(
                     vscode.workspace.workspaceFolders !== undefined &&
                     PortalWebView.currentPanel &&
                     PortalWebView.currentDocument !==
-                        vscode.window.activeTextEditor.document.fileName &&
+                    vscode.window.activeTextEditor.document.fileName &&
                     PortalWebView.checkDocumentIsHTML()
                 ) {
                     PortalWebView.currentPanel._update();
@@ -99,7 +100,7 @@ export async function activate(
                     vscode.workspace.workspaceFolders !== undefined &&
                     PortalWebView.currentPanel &&
                     PortalWebView.currentDocument ===
-                        vscode.window.activeTextEditor.document.fileName
+                    vscode.window.activeTextEditor.document.fileName
                 ) {
                     PortalWebView.currentPanel._update();
                 }
