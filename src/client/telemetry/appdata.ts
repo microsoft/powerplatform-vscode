72510f4f9924405ad69efb4992e4904a2aab67e7
Joe Mayo (MSFT) <joem-msft@users.noreply.github.com>
2021-05-25 16:25:09 -0700

    Integrate with App Insights telemetry (#48)
    * Integrate with App Insights telemetry

---
+++ src/client/telemetry/appdata.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as process from "process";
+import * as os from "os";
+
+export function getAppDataPath(): string {
+    const platform = os.platform();
+    switch (platform) {
+        case 'darwin':
+            return '~/Library/';
+        case 'linux':
+            return `${process.env.HOME}/.config/`;
+        case 'win32':
+            return process.env.LOCALAPPDATA as string;
+        default:
+            throw new Error(`Platform "${platform}" is not currently supported`);
+    }
+}
---
+++ src/client/telemetry/localfileusersettings.ts
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as fs from 'fs-extra';
+import { getAppDataPath } from './appdata';
+import * as path from 'path';
+import { v4 } from 'uuid';
+
+interface IUserSettings {
+    readonly uniqueId: string;
+    readonly settingVersion: string;
+    readonly telemetryEnabled: boolean;
+}
+
+const settingVersionId = '1.0';
+const userSettingsFileName = 'usersettings.json';
+let settings: IUserSettings = {
+    uniqueId: v4(),
+    settingVersion: settingVersionId,
+    telemetryEnabled: true
+};
+
+// Application Path and User settings file name should be consistent with CLI
+export function readUserSettings(applicationPath?: string | undefined): IUserSettings {
+    if (!applicationPath) {
+        applicationPath = path.join(getAppDataPath(), 'Microsoft', 'PowerAppsCli');
+    }
+    const userSettingFilePath = path.join(applicationPath, userSettingsFileName);
+    if (fs.existsSync(userSettingFilePath)) {
+        const stream = fs.readFileSync(userSettingFilePath, 'utf8');
+        try {
+            settings = JSON.parse(stream) as IUserSettings;
+            if (settings.uniqueId === '') {
+                writeUserSettings(settings, applicationPath);
+            }
+        } catch (err) {
+            try {
+                fs.unlinkSync(userSettingFilePath);
+            } catch {
+                // Ignore error on delete
+            }
+            writeUserSettings(settings, applicationPath);
+        }
+    } else {
+        writeUserSettings(settings, applicationPath);
+    }
+    return settings;
+}
+
+function writeUserSettings(userSettings: IUserSettings, applicationPath: string): void {
+    userSettings = userSettings || settings;
+    try {
+        fs.ensureDirSync(applicationPath);
+        fs.writeFileSync(path.join(applicationPath, userSettingsFileName), JSON.stringify(userSettings));
+    } catch (err) {
+        // catch err
+    }
+}
