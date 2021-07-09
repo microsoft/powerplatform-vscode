643b5d946a89a6ff74d1c9d8b023fc86362988db
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2021-07-09 13:37:51 -0700

    Added a blocking queue to permite consumption of async/event returned items (#71)

---
+++ src/common/utilities/BlockingQueue.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Queue that returns Promises on dequeue, either already resolved if the item
+// has already been enqueued, or still waiting if the item has not yet arrived.
+export class BlockingQueue<T> {
+    private readonly promiseQueue: Promise<T>[] = [];
+    private readonly resolverQueue: ((t: T) => void)[] = [];
+
+    private addPromiseResolverPair() {
+        this.promiseQueue.push(
+            new Promise(resolve => {this.resolverQueue.push(resolve); })
+        );
+    }
+
+    enqueue(t: T) : void {
+        // ensure that either a resolver is already queued, or generate a new pair
+        if (!this.resolverQueue.length) {
+            this.addPromiseResolverPair();
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const resolve = this.resolverQueue.shift()!;
+        resolve(t);
+    }
+
+    dequeue() : Promise<T> {
+        // ensure that either a promise is already queued, or generate a new pair
+        if (!this.promiseQueue.length) {
+            this.addPromiseResolverPair();
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const promise = this.promiseQueue.shift()!;
+        return promise;
+    }
+}
