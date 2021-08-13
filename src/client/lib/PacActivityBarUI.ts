b367cf24913cf96c54ab3e8e94c20361b358054c
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2021-08-12 23:46:17 -0700

    Minor UI refactor (#98)

--- src/client/lib/PacActivityBarUI.ts
+++ src/client/lib/PacActivityBarUI.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import * as vscode from 'vscode';
-import { AdminEnvironmentListing, AuthProfileListing, SolutionListing, PacOutputWithResultList } from '../pac/PacTypes';
 import { PacWrapper } from '../pac/PacWrapper';
+import { PacFlatDataView, SolutionTreeItem, AdminEnvironmentTreeItem, AuthProfileTreeItem } from './PanelComponents';
 
 export function RegisterPanels(pacWrapper: PacWrapper): vscode.Disposable[] {
     const registrations: vscode.Disposable[] = [];
@@ -66,69 +66,4 @@ export function RegisterPanels(pacWrapper: PacWrapper): vscode.Disposable[] {
         }));
 
     return registrations;
-}
-
-class PacFlatDataView<PacResultType, TreeType extends vscode.TreeItem> implements vscode.TreeDataProvider<TreeType> {
-    private _onDidChangeTreeData: vscode.EventEmitter<TreeType | undefined | void> = new vscode.EventEmitter<TreeType | undefined | void>();
-    readonly onDidChangeTreeData: vscode.Event<TreeType | undefined | void> = this._onDidChangeTreeData.event;
-
-    constructor(
-        public readonly dataSource: () => Promise<PacOutputWithResultList<PacResultType>>,
-        private readonly mapToTreeItem: (item: PacResultType) => TreeType,
-        private readonly itemFilter?: (item:PacResultType) => boolean) {
-    }
-
-    refresh(): void {
-        this._onDidChangeTreeData.fire();
-    }
-
-    public getTreeItem(element: TreeType): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return element;
-    }
-
-    public async getChildren(element?: TreeType): Promise<TreeType[]> {
-        if (element) {
-            // If we are not at the root, then there are no child items
-            return [];
-        } else {
-            const pacOutput = await this.dataSource();
-            if (pacOutput && pacOutput.Status === "Success" && pacOutput.Results) {
-                const items = pacOutput.Results
-                    .filter(this.itemFilter || (_ => true))
-                    .map(this.mapToTreeItem);
-                return items;
-            } else {
-                return [];
-            }
-        }
-    }
-}
-
-class AuthProfileTreeItem extends vscode.TreeItem {
-    public constructor(public readonly model: AuthProfileListing) {
-        super(AuthProfileTreeItem.createLabel(model), vscode.TreeItemCollapsibleState.None);
-        if (model.IsActive){
-            this.iconPath = new vscode.ThemeIcon("star-full")
-        }
-    }
-    private static createLabel(profile: AuthProfileListing): string {
-        if (profile.Name) {
-            return `${profile.Kind}: ${profile.Name}`;
-        } else if (profile.Kind === "ADMIN") {
-            return `${profile.Kind}: ${profile.User}`;
-        } else {
-            return `${profile.Kind}: ${profile.User} - ${profile.Resource}`;
-        }
-    }
-}
-class SolutionTreeItem extends vscode.TreeItem {
-    public constructor(public readonly model: SolutionListing) {
-        super(`${model.FriendlyName}, Version: ${model.VersionNumber}`);
-    }
-}
-
-class AdminEnvironmentTreeItem extends vscode.TreeItem {
-    public constructor(public readonly model: AdminEnvironmentListing) {
-        super(`${model.DisplayName} ${model.EnvironmentUrl}`);
-    }
 }
