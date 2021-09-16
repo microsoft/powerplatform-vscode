12f480cc23d623dbe4de5c534bb18e97a655ac7d
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2021-09-16 14:53:04 -0700

    WIP

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -103,13 +103,20 @@ export async function activate(
         );
     }
 
-    // Only register the server-side commands if either 
+    // Only register the server-side commands if either
     // 1) UI and Server are running at the same place (vscode.env.remoteName === undefined)
     // 2) we are on the server side of a split extension (vscode.ExtensionKind === workspace)
     const extension = vscode.extensions.getExtension('microsoft-IsvExpTools.powerplatform-vscode');
+    const crashDebug = vscode.window.createOutputChannel("CrashDebug");
+    crashDebug.show();
+    crashDebug.appendLine(`Remote Name: ${vscode.env.remoteName}`);
+    crashDebug.appendLine(`Extension Kind: ${extension?.extensionKind}`);
     if (vscode.env.remoteName === undefined || extension && extension.extensionKind !== vscode.ExtensionKind.UI) {
-        const cli = new CliAcquisition(new CliAcquisitionContext(_context, _telemetry));
+        crashDebug.appendLine(`CLI Acquire - start`);
+        const cli = new CliAcquisition(new CliAcquisitionContext(_context, _telemetry, crashDebug));
+        crashDebug.appendLine(`CLI Acquire - constructor finished`);
         const cliPath = await cli.ensureInstalled();
+        crashDebug.appendLine(`CLI Acquire - ensure installed finished`);
         _context.subscriptions.push(cli);
         _context.subscriptions.push(new PacTerminal(_context, _telemetry, cliPath));
     }
@@ -258,7 +265,8 @@ function isCurrentDocumentEdited() : boolean{
 class CliAcquisitionContext implements ICliAcquisitionContext {
     public constructor(
         private readonly _context: vscode.ExtensionContext,
-        private readonly _telemetry: ITelemetry) {
+        private readonly _telemetry: ITelemetry,
+        public readonly debugOutputChannel: vscode.OutputChannel) {
     }
 
     public get extensionPath(): string { return this._context.extensionPath; }
@@ -267,8 +275,10 @@ class CliAcquisitionContext implements ICliAcquisitionContext {
 
     showInformationMessage(message: string, ...items: string[]): void {
         vscode.window.showInformationMessage(message, ...items);
+        this.debugOutputChannel.appendLine(message);
     }
     showErrorMessage(message: string, ...items: string[]): void {
         vscode.window.showErrorMessage(message, ...items);
+        this.debugOutputChannel.appendLine(message);
     }
 }
--- src/client/lib/CliAcquisition.ts
+++ src/client/lib/CliAcquisition.ts
@@ -10,6 +10,7 @@ import * as os from 'os';
 import { Extract } from 'unzip-stream'
 import { ITelemetry } from '../telemetry/ITelemetry';
 import find from 'find-process';
+import { spawnSync } from 'child_process';
 
 // allow for DI without direct reference to vscode's d.ts file: that definintions file is being generated at VS Code runtime
 export interface ICliAcquisitionContext {
@@ -61,17 +62,22 @@ export class CliAcquisition implements IDisposable {
 
     async installCli(pathToNupkg: string): Promise<string> {
         const pacToolsPath = path.join(this._cliPath, 'tools');
-        if (this.isCliExpectedVersion()) {
-            return Promise.resolve(pacToolsPath);
-        }
+        // if (this.isCliExpectedVersion()) {
+        //     return Promise.resolve(pacToolsPath);
+        // }
         // nupkg has not been extracted yet:
         this._context.showInformationMessage(`Preparing pac CLI (v${this.cliVersion})...`);
+        this._context.showInformationMessage("CliAcquisition.installCli() - killing processes");
         await this.killProcessesInUse(pacToolsPath);
+        this._context.showInformationMessage("CliAcquisition.installCli() - killed processes");
+        this._context.showInformationMessage("CliAcquisition.installCli() - dir sync start");
         fs.emptyDirSync(this._cliPath);
+        this._context.showInformationMessage("CliAcquisition.installCli() - dir sync end");
         return new Promise((resolve, reject) => {
             fs.createReadStream(pathToNupkg)
                 .pipe(Extract({ path: this._cliPath }))
                 .on('close', () => {
+                    this._context.showInformationMessage("CliAcquisition.installCli() - Pac CLI installed");
                     this._context.telemetry.sendTelemetryEvent('PacCliInstalled', { cliVersion: this.cliVersion });
                     this._context.showInformationMessage('The pac CLI is ready for use in your VS Code terminal!');
                     if (os.platform() !== 'win32') {
@@ -80,6 +86,7 @@ export class CliAcquisition implements IDisposable {
                     this.setInstalledVersion(this._cliVersion);
                     resolve(pacToolsPath);
                 }).on('error', (err: unknown) => {
+                    this._context.showInformationMessage(`CliAcquisition.installCli() - Install Error ${err}`);
                     this._context.showErrorMessage(`Cannot install pac CLI: ${err}`);
                     reject(err);
                 })
@@ -95,9 +102,76 @@ export class CliAcquisition implements IDisposable {
     }
 
     async killProcessesInUse(pacInstallPath: string): Promise<void> {
-        const list = (await find('name', 'pacTelemetryUpload', true)).concat(await find('name', 'pac', true));
-        list.filter(info => info.cmd.startsWith(pacInstallPath))
-            .forEach(info => process.kill(info.pid));
+
+        this._context.showInformationMessage("CliAcquisition.killProcessesInUse() - Searching for processes");
+        const tel = await this.findProcessWithRetry(pacInstallPath, 'pacTelemetryUpload');
+        const pac = await this.findProcessWithRetry(pacInstallPath, 'pac');
+        this._context.showInformationMessage("CliAcquisition.killProcessesInUse() - PAC Processes search done");
+        const list = tel.concat(pac);
+
+        const debugOutput = list.map(info => `PID: ${info.pid}\tcmd: ${info.cmd}`).join("\n\t");
+        this._context.showInformationMessage(`CliAcquisition.killProcessesInUse() - Processes found:\n\t${debugOutput}`)
+
+        //const list = (await find('name', 'pacTelemetryUpload', true)).concat(await find('name', 'pac', true));
+        this._context.showInformationMessage(`CliAcquisition.killProcessesInUse() - Found processes ${list}`);
+        list.forEach(info => process.kill(info.pid));
+    }
+
+    async findProcessWithRetry(pacInstallPath: string, procName: string): Promise<{pid: number, cmd: string, ppid?: number|undefined, uid?: number|undefined, gid?: number|undefined}[]> {
+        try {
+            this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - Attempt find-process ${procName}`);
+            const processes = await find('name', procName, true);
+            this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - Attempt find-process ${procName} - Success!`);
+            return processes.filter(info => info.cmd.includes(pacInstallPath));
+        } catch (err) {
+            this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - Attempt find-process ${procName} - Fail!  Error was ${err}`);
+
+            // TODO - Guard on error including "screen size is bogus", and Linux OS check
+            if (typeof err === "string" && err.includes("screen size is bogus")) {
+
+                try {
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - Explicit PS attempt [${procName}] in location [${pacInstallPath}]`);
+                    const psResult = spawnSync("ps", ["ax","-ww","-o","pid,args"]);
+                    if (psResult.error) {
+                        this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - Explicit PS attempt ${procName} - Error: ${psResult.error}`);
+                    }
+
+                    const foo = psResult.output
+                        .filter(chunk => chunk)
+                        .map((chunk: string) => chunk.split("\n"))
+                        .reduce((accumulator, current) => accumulator.concat(current), [])
+                        .filter(line => line);
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - lines: ${foo?.length ?? -1} foo: ${foo}`);
+                    //const foo1 = foo.filter(line => line && line.includes(path.join(pacInstallPath, procName)));
+                    const foo1 = foo.filter(line => line && line.includes(pacInstallPath));
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - lines: ${foo1?.length ?? -1} foo1: ${foo1?.join("\n")}`);
+                    const foo2 = foo1.map(line => {
+                        this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - type before trim: ${typeof line}`);
+                        return line.trim();
+                    });
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - lines: ${foo2?.length ?? -1} foo2: ${foo2?.join("\n")}`);
+                    const foo3 = foo2.map(line => line.split(' ', 1));
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - foo3: ${foo3}`);
+                    const foo4 = foo3.map(split => ({pid: parseInt(split[0]), cmd: split[1]}));
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - foo4: ${foo4}`);
+
+                    // const processes = psResult.output
+                    //     .filter(line => line && line.includes(path.join(pacInstallPath, procName)))
+                    //     .map(line => line.trim())
+                    //     .map(line => line.split(' ', 1))
+                    //     .map(split => ({pid: parseInt(split[0]), cmd: split[1]}));
+
+                    // return processes;
+                    return [];// foo4
+                } catch (retryErr) {
+                    this._context.showInformationMessage(`CliAcquisition.findProcessWithRetry() - failed on second attempt to find ${procName} with error ${retryErr}.`)
+                    throw retryErr;
+                }
+            }
+
+            this._context.showErrorMessage("Error finding processes");
+            throw err;
+        }
     }
 
     getLatestNupkgVersion(): string {
@@ -148,3 +222,4 @@ export class CliAcquisition implements IDisposable {
         }
     }
 }
+
