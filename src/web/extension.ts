79e58840927232b849498fa2f77587cd9cc29e05
Ashish Choudhary <aschoud@microsoft.com>
2022-01-30 20:56:24 +0530

    initial changes to add webextension to the existing node extension

---
+++ src/web/extension.ts
@@ -0,0 +1,214 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import { PortalsFS } from './fileSystemProvider';
+
+const portalsFolderName = 'portal';
+const portalsUriScheme = 'portals';
+const portalsWorkspaceName = 'Power Portals';
+let fileName = '';
+
+
+export function activate(context: vscode.ExtensionContext): void {
+
+    console.log('Hello from - Portals WebExtension');
+
+    const portalsFS = new PortalsFS();
+    context.subscriptions.push(vscode.workspace.registerFileSystemProvider(portalsUriScheme, portalsFS, { isCaseSensitive: true }));
+    let initialized = false;
+    let accessToken = '';
+
+
+    context.subscriptions.push(vscode.commands.registerCommand('portals.init', async (args: any) => {
+        if (initialized) {
+            return;
+        }
+        initialized = true;
+
+        if (!args) {
+            return;
+        }
+
+        const { dataverseOrg, api, data, version, entity, entityId } = args;
+
+        vscode.window.showInformationMessage('editing '+ entity);
+
+        // uncomment this after pre-authorization of VSCode firstPartyApp(aebc6443-996d-45c2-90f0-388ff96faa56) in Dataverse
+        // try {
+        //     const session = await vscode.authentication.getSession("microsoft", ["https://org2e2e9cae.crm.dynamics.com" + "/.default"], { createIfNone: true });
+        //     console.log(session.accessToken);
+        // }catch(e: any) {
+        //     console.log(e.toString());
+        // }
+
+        // some more files & folders
+        portalsFS.createDirectory(vscode.Uri.parse(`${portalsUriScheme}:/${portalsFolderName}/`));
+
+        // Do something before delay
+        vscode.window.showInformationMessage('fetching portal data...');
+        accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCIsImtpZCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCJ9.eyJhdWQiOiJodHRwczovL29yZzJlMmU5Y2FlLmNybS5keW5hbWljcy5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kMzFkZDk5Yy02NWIyLTRkYjUtOWIzOS1jNDliYzllZjA5ZWQvIiwiaWF0IjoxNjQyMDM0OTczLCJuYmYiOjE2NDIwMzQ5NzMsImV4cCI6MTY0MjAzOTQ0OSwiYWNyIjoiMSIsImFpbyI6IkUyWmdZR2lQVVRNdTJmeFdMOFkvV3piSzk4YWZEQW0xMmR2L3paY3oyNUUwODZTS3Z4WUEiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiMTViNmZiMjMtNjA4YS00ODUxLWIyNWItZDA2YjQ0ZTdjNzdmIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJDaG91ZGhhcnkiLCJnaXZlbl9uYW1lIjoiQXNoaXNoIiwiaXBhZGRyIjoiNDkuMjA3LjE5NC4xMTciLCJuYW1lIjoiQXNoaXNoIENob3VkaGFyeSIsIm9pZCI6ImIxNWU5MTA2LWNjMjAtNDM2Ni1iZTI3LTQzNDVkNDBiZjE4OCIsInB1aWQiOiIxMDAzMjAwMUNBRjE4QTIwIiwicmgiOiIwLkFVWUFuTmtkMDdKbHRVMmJPY1NieWU4SjdTUDd0aFdLWUZGSXNsdlFhMFRueDMtQUFPNC4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJPdHJySWRKNlo3NFlQMlRpUmpkeXdGdEZvRlpQYlhIX0tXcms4NnJhZ0cwIiwidGlkIjoiZDMxZGQ5OWMtNjViMi00ZGI1LTliMzktYzQ5YmM5ZWYwOWVkIiwidW5pcXVlX25hbWUiOiJBc2hpc2hDaG91ZGhhcnlAdnNjb2RlUG9jLm9ubWljcm9zb2Z0LmNvbSIsInVwbiI6IkFzaGlzaENob3VkaGFyeUB2c2NvZGVQb2Mub25taWNyb3NvZnQuY29tIiwidXRpIjoiUzBIOXlIREFMVVdfWThhSlhrQ1RBQSIsInZlciI6IjEuMCJ9.gw2Cyy3KSIzbZe9Df16rij8N2Qt1Z8Ycru5EU6uTwNkEmOCzwAij1XkGJWH4Ezyz0r_dgU6CQGm5Kp2bL097tzRHGRzlZGHZ-SpUzUxuesrY6HeqwCyt1SBAGnnKDV0KMi5IBRLES8kGhncgniyvO-zAXYDLQ3R6Lv1RUWO9gTSFZzx0KP6ZRsOpLo32LoIjRu0hDOKnACoZGNvRlJNgAXYxgM8BVPzPwPmeXcvKv6-ULa0GwTClBpVCW8UykQHTuqZukkp87vLp61bkXDdfvEEyerKo6GI2GoARuyrNOl7we9PKpJbqKx5QvQzwqZkDbhtDO4nFeLz_xpQwfL0XBA';
+
+        // Prompt for the Access Token.
+        const token = await vscode.window.showInputBox({
+            ignoreFocusOut: true,
+            placeHolder: 'Portal/Dataverse access token',
+            prompt: 'Enter a Portal/Dataverse access token.'
+        });
+        if (!token) {
+            throw new Error('AccessToken is required');
+        }
+        accessToken = token;
+
+        vscode.window.showInformationMessage('received access token!');
+
+        let adxCopy = '';
+        let adxCustomJavascript = '';
+        let adxCustomCss = '';
+        let adxSource = '';
+
+        try {
+            const requestUrl = getRequestUrl('GET', dataverseOrg, api, data, version, entity, entityId);
+            vscode.window.showInformationMessage(requestUrl);
+            const req = await fetch(requestUrl, {
+                headers: getHeader(accessToken),
+            });
+            if (!req.ok) {
+                throw new Error(req.statusText);
+            }
+            const res = await req.json();
+            console.log(res);
+            if (res) {
+                fileName = res['adx_name'] ? res['adx_name'] : '';
+                adxCopy = res['adx_copy'] ? res['adx_copy'] : '';
+                adxCustomJavascript = res['adx_customjavascript'] ? res['adx_customjavascript'] : '';
+                adxCustomCss = res['adx_customcss'] ? res['adx_customcss'] : '';
+                adxSource = res['adx_source'] ? res['adx_source'] : '';
+            }
+            if (entity === 'adx_webpages') {
+                portalsFS.writeFile(vscode.Uri.parse(`${portalsUriScheme}:/${portalsFolderName}/${fileName}.css`), new TextEncoder().encode(adxCustomCss), { create: true, overwrite: true });
+                portalsFS.writeFile(vscode.Uri.parse(`${portalsUriScheme}:/${portalsFolderName}/${fileName}.js`), new TextEncoder().encode(adxCustomJavascript), { create: true, overwrite: true });
+                portalsFS.writeFile(vscode.Uri.parse(`${portalsUriScheme}:/${portalsFolderName}/${fileName}.html`), new TextEncoder().encode(adxCopy), { create: true, overwrite: true });
+            } else if (entity === 'adx_webtemplates') {
+                portalsFS.writeFile(vscode.Uri.parse(`${portalsUriScheme}:/${portalsFolderName}/${fileName}.html`), new TextEncoder().encode(adxSource), { create: true, overwrite: true });
+            }
+
+            vscode.workspace.onDidSaveTextDocument(async (e) => {
+                vscode.window.showInformationMessage('saving file: ' + e.uri);
+                const newFileData = portalsFS.readFile(e.uri);
+                console.log(new TextDecoder().decode(newFileData));
+
+                const patchRequestUrl = getRequestUrl('PATCH', dataverseOrg, api, data, version, entity, entityId);
+                vscode.window.showInformationMessage(patchRequestUrl);
+                await saveCodeInDataverse(accessToken, patchRequestUrl, e.uri, entity, new TextDecoder().decode(newFileData));
+            });
+        } catch (e: any) {
+            if (e.message === 'Unauthorized') {
+                vscode.window.showErrorMessage('Failed to authenticate');
+            }
+            throw e;
+        }
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('portals.workspaceInit', async () => {
+        vscode.workspace.updateWorkspaceFolders(0, null, { uri: vscode.Uri.parse(`${portalsUriScheme}:/`), name: portalsWorkspaceName });
+    }));
+
+    // this is just for testing...remove this once pre-authorization is completed.
+    context.subscriptions.push(vscode.commands.registerCommand('portals.login', async () => {
+        try {
+            const session = await vscode.authentication.getSession("microsoft", ["https://org2e2e9cae.crm.dynamics.com" + "/.default"], { createIfNone: true });
+            console.log(session.accessToken);
+        } catch (e: any) {
+            console.log(e.toString());
+        }
+    }));
+
+    // this is used for dynamically updating access token. Remove this after pre-authorization work is complete.
+    context.subscriptions.push(vscode.commands.registerCommand('portals.accessToken', async () => {
+        // Prompt for the Access Token.
+        const token = await vscode.window.showInputBox({
+            ignoreFocusOut: true,
+            placeHolder: 'Portal/Dataverse access token',
+            prompt: 'Enter a Portal/Dataverse access token.'
+        });
+        if (!token) {
+            throw new Error('AccessToken is required');
+        }
+        accessToken = token;
+    }));
+
+}
+
+async function saveCodeInDataverse(accessToken: string, requestUrl: string, fileUri: vscode.Uri, entity: string, value: string) {
+    let requestBody = '';
+
+    const fileExtensionRX = /(?<extension>\.[0-9a-z]+$)/i;
+    const fileExtensionMatch = fileExtensionRX.exec(fileUri.path);
+
+    if (fileExtensionMatch?.groups === undefined) {
+        return undefined;
+    }
+
+    const { extension } = fileExtensionMatch.groups;
+
+    console.log(extension);
+
+    switch (extension) {
+        case '.html':
+            if (entity === "adx_webpages") {
+                requestBody = JSON.stringify({ "adx_copy": value });
+            }
+            else if (entity === "adx_webtemplates") {
+                requestBody = JSON.stringify({ "adx_source": value });
+            }
+            break;
+        case '.js':
+            requestBody = JSON.stringify({ "adx_customjavascript": value });
+            break;
+        case '.css':
+            requestBody = JSON.stringify({ "adx_customcss": value });
+            break;
+        default:
+            break;
+    }
+
+    if (requestBody) {
+        await fetch(requestUrl, {
+            method: 'PATCH',
+            headers: getHeader(accessToken),
+            body: requestBody
+        });
+    }
+}
+
+function getHeader(accessToken: string) {
+    return {
+        authorization: "Bearer " + accessToken,
+        'content-type': "application/json; charset=utf-8",
+        accept: "application/json",
+        'OData-MaxVersion': "4.0",
+        'OData-Version': "4.0",
+    };
+}
+
+function getRequestUrl(method: string, dataverseOrg: string, api: string, data: string, version: string, entity: string, entityId: string): string {
+    let requestUrl = '';
+    switch (method) {
+        case 'GET':
+            // TODO: move these checks to a common function
+            if (entity === 'adx_webpages') {
+                requestUrl = `https://${dataverseOrg}/${api}/${data}/${version}/${entity}(${entityId})?$select=adx_name,adx_copy,adx_customcss,adx_customjavascript`;
+            }
+            else if (entity === 'adx_webtemplates') {
+                requestUrl = `https://${dataverseOrg}/${api}/${data}/${version}/${entity}(${entityId})?$select=adx_name,adx_source`;
+            }
+            break;
+        case 'PATCH':
+            requestUrl = `https://${dataverseOrg}/${api}/${data}/${version}/${entity}(${entityId})`;
+            break;
+
+        default:
+            break;
+    }
+    return requestUrl;
+}
