d7940aecdb97bd31a1cad9abb128fb31afacfb98
Ashish Choudhary <aschoud@microsoft.com>
2022-05-24 17:28:49 +0530

    modified tsconfig for webExtension

--- webpack.config.js
+++ webpack.config.js
@@ -57,8 +57,7 @@ const webExtensionConfig = {
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
-		'extension': './src/web/extension.ts',
-		'test/suite/index': './src/web/test/suite/index.ts'
+		'extension': './src/web/extension.ts'
 	},
 	output: {
 		filename: '[name].js',
