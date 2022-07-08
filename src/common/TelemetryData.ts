1c9cba113ba2ff6249e5a39262808c1cbd391fe2
Felix Schober <github@felix-schober.de>
2022-07-08 09:59:52 +0100

    add common header definition rule (#230)
    Co-authored-by: Felix Schober <feschobe@microsoft.com>

--- src/common/TelemetryData.ts
+++ src/common/TelemetryData.ts
@@ -1,7 +1,7 @@
-/* --------------------------------------------------------------------------------------------
+/*
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
+ */
 
  /**
   * This type is used in the contract of telemetry/event notifications sent from language servers to the language clients.
