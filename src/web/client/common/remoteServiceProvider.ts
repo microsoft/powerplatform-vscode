6992806f39341fb4f69d72ddb26792d3ed36143f
Ved Garbha <vedgarbha@microsoft.com>
2022-07-09 13:27:39 +0530

    updated header for the files

--- src/web/client/common/remoteServiceProvider.ts
+++ src/web/client/common/remoteServiceProvider.ts
@@ -1,3 +1,7 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use strict';
 import * as vscode from 'vscode';
