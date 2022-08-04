1f7816c703c5a318fa102cd58e94e560b46d8ee3
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2022-08-03 17:31:30 -0700

    Updated Dev telemetry key, and added comments

--- src/common/telemetry/generated/telemetryConfiguration.ts
+++ src/common/telemetry/generated/telemetryConfiguration.ts
@@ -3,4 +3,4 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-export const AI_KEY = "0d422197-d351-41c5-b371-a270ca3b13af"; // dev
+export const AI_KEY = '441859ca-4501-4653-bc1d-65fb8663108a'; // capisvtoolsappinsights-dev
--- src/common/telemetry/telemetryConfigurationProd.ts
+++ src/common/telemetry/telemetryConfigurationProd.ts
@@ -3,4 +3,4 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-export const AI_KEY = "0d422197-d351-41c5-b371-a270ca3b13af"; // prod
+export const AI_KEY = '0d422197-d351-41c5-b371-a270ca3b13af'; // IsvTooling_AI_WUS2
