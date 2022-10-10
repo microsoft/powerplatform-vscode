96b617589c66fe565bca6dc6ab8edf33ab433b7b
David JENNI <3200210+davidjenni@users.noreply.github.com>
2022-10-10 13:59:39 -0700

    Sept refresh: pac CLI 1.19.x (#310)
    Co-authored-by: david@DAVID-PC <davidjen@microsoft.com>

--- gulpfile.js
+++ gulpfile.js
@@ -292,7 +292,7 @@ async function snapshot() {
 }
 
 const feedName = 'CAP_ISVExp_Tools_Stable';
-const cliVersion = '1.18.4';
+const cliVersion = '1.19.3';
 
 const recompile = gulp.series(
     clean,
