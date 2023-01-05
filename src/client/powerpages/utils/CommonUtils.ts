ffe72a65bf6c488978b8cd6a6e8b099f181515ae
amitjoshi <amitjoshi@microsoft.com>
2023-01-05 18:56:00 +0530

    content snippet integration

---
+++ src/client/powerpages/utils/CommonUtils.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+export const isNullOrEmpty = (str: string | undefined): boolean => {
+    return !str || str.trim().length === 0;
+};
+
+export const folderName = (name: string) => {
+    return name.replace(/[/ ]/g, "-").toLowerCase();
+};
+
+export const fileName = (name: string) => {
+    const words = name.split(/[/ ]/);
+
+    // Uppercase the first letter of each word and join the words back together
+    return words.map((word) => word[0].toUpperCase() + word.slice(1)).join("-");
+};
