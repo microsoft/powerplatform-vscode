d447e47cd8fd22cd3dde045b59bb8b3163305ed0
Vipin Bhandari <v-vbhanndari@microsoft.com>
2023-01-12 09:39:44 +0530

    remove extra space

--- src/web/client/test/integration/schemaHelperUtil.test.ts
+++ src/web/client/test/integration/schemaHelperUtil.test.ts
@@ -693,7 +693,6 @@ describe("schemaHelperUtil_ShouldReturnportalSchemaV2", () => {
             [null, null],
         ]);
         const schema = "portalschemav2".toUpperCase();
-        
         //Action
         const resultData = getwebsiteLanguageIdToPortalLanguageMap(
             result,
