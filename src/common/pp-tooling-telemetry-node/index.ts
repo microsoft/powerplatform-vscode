7f5249199fa388c68073536928d6ab2e4d09e36b
Joe Mayo <joemay@microsoft.com>
2023-01-12 14:57:51 -0800

    Copy pp-tooling-telemetry-node

---
+++ src/common/pp-tooling-telemetry-node/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+// This module represents types with parallels in the BatchedTelemetry libraries found in the CRM.DevToolsCore repo.
+
+export * from './interfaces';
+export * from './AppInsightsResource';
+export * from './AppInsightsResourceProvider';
+export * from './TelemetryUserSettingsFileProvider';
+export * as TelemetryConstants from './TelemetryConstants';
+export * as AppInsightsConfigUtility from './AppInsightsConfigUtility';
+export * as AppTelemetryConfigUtility from './AppTelemetryConfigUtility';
+export * as AppTelemetryUtility from './AppTelemetryUtility';
