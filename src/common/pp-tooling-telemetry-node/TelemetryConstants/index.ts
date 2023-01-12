39b83e0542893fb7cf092ff9ad0747f943c9861d
Joe Mayo <joemay@microsoft.com>
2023-01-12 14:57:51 -0800

    Copy pp-tooling-telemetry-node

---
+++ src/common/pp-tooling-telemetry-node/TelemetryConstants/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+// This module represents types with parallels in the BatchedTelemetry libraries found in the CRM.DevToolsCore repo.
+
+export * as PropertyNames from './PropertyNames';
+export * as EnvironmentVariableNames from './EnvironmentVariableNames';
