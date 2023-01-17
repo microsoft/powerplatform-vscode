f08bc72c0752adfb83ffea203922fb9411b0d8fa
amitjoshi <amitjoshi@microsoft.com>
2023-01-17 20:09:31 +0530

    refactor

--- src/client/powerpages/Contentsnippet.ts
+++ src/client/powerpages/Contentsnippet.ts
@@ -15,49 +15,41 @@ import { QuickPickItem } from "vscode";
 import { MultiStepInput } from "./utils/MultiStepInput";
 
 export const contentSnippet = async (context: vscode.ExtensionContext) => {
-    const contentSnippetInputs = await myMultiStepInput();
+    const { name, type } = await myMultiStepInput();
 
-    const contentSnippetName = contentSnippetInputs.name;
-    if (!isNullOrEmpty(contentSnippetName)) {
+    if (!isNullOrEmpty(name)) {
         const terminal = vscode.window.createTerminal("Powerpages", "");
-        terminal.sendText(
-            `cd data\n ../node_modules/.bin/yo @microsoft/powerpages:contentsnippet "${contentSnippetName}" "${contentSnippetInputs.type}"`
+        terminal.sendText(`cd data\n ../node_modules/.bin/yo @microsoft/powerpages:contentsnippet "${name}" "${type}"`);
+
+        const folder = folderName(name);
+        const file = fileName(name);
+
+        const watcher = vscode.workspace.createFileSystemWatcher(
+            new vscode.RelativePattern(
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                vscode.workspace.workspaceFolders![0],
+                `**/content-snippets/${folder}/${file}.en-US.contentsnippet.yml`
+            ),
+            false,
+            true,
+            true
         );
-
-        const folder = folderName(contentSnippetName);
-        const file = fileName(contentSnippetName);
-
-        const watcher: vscode.FileSystemWatcher =
-            vscode.workspace.createFileSystemWatcher(
-                new vscode.RelativePattern(
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                    vscode.workspace.workspaceFolders![0],
-                    `**/content-snippets/${folder}/${file}.en-US.contentsnippet.yml` //TODO: check for text/html file
-                ),
-                false,
-                true,
-                true
-            );
 
         context.subscriptions.push(watcher);
-        vscode.window.withProgress(
-            {
-                location: vscode.ProgressLocation.Notification,
-                cancellable: true,
-                title: localize(
-                    "microsoft-powerapps-portals.webExtension.contentsnippet.create.message",
-                    "Creating Content Snippet..."
-                ),
-            },
-            async () => {
-                watcher.onDidCreate(async (uri) => {
-                    await vscode.window.showTextDocument(uri);
-                    terminal.dispose();
-                });
-            }
-        );
+
+        vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            cancellable: true,
+            title: localize("microsoft-powerapps-portals.webExtension.contentsnippet.create.message", "Creating Content Snippet..."),
+        }, async () => {
+            watcher.onDidCreate(async (uri) => {
+                await vscode.window.showTextDocument(uri);
+                terminal.dispose();
+            });
+        });
     }
 };
+
 
 async function myMultiStepInput() {
     const contentSnippetTypes: QuickPickItem[] = ["html", "text"].map(
