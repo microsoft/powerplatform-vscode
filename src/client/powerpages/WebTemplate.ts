31e16b6bf298d08a18753e45656546eef3943c4f
amitjoshi <amitjoshi@microsoft.com>
2023-01-31 10:40:29 +0530

    child process terminal

--- src/client/powerpages/WebTemplate.ts
+++ src/client/powerpages/WebTemplate.ts
@@ -4,29 +4,41 @@
  */
 
 import * as vscode from "vscode";
-import { isNullOrEmpty } from "./utils/CommonUtils";
-import * as nls from 'vscode-nls';
-nls.config({ messageFormat: nls.MessageFormat.bundle, bundleFormat: nls.BundleFormat.standalone })();
+import {
+    formatFileName,
+    formatFolderName,
+    isNullOrEmpty,
+} from "./utils/CommonUtils";
+import * as nls from "vscode-nls";
+import { exec } from "child_process";
+import path from "path";
+nls.config({
+    messageFormat: nls.MessageFormat.bundle,
+    bundleFormat: nls.BundleFormat.standalone,
+})();
 const localize: nls.LocalizeFunc = nls.loadMessageBundle();
 
 export const webTemplate = (context: vscode.ExtensionContext) => {
     vscode.window
-        .showInputBox({ placeHolder: localize("microsoft-powerapps-portals.webExtension.webtemplate.name","Enter the name of the web template") })
+        .showInputBox({
+            placeHolder: localize(
+                "microsoft-powerapps-portals.webExtension.webtemplate.name",
+                "Enter the name of the web template"
+            ),
+        })
         .then((value) => {
-            let webTemplateName = value;
-            if (!isNullOrEmpty(webTemplateName)) {
-                webTemplateName = webTemplateName?.split(' ').join('-');
-                const terminal = vscode.window.createTerminal("Powerpages", "");
-                terminal.sendText(
-                    `cd data\n ../node_modules/.bin/yo @microsoft/powerpages:webtemplate ${webTemplateName}`
-                );
-
+            if (!isNullOrEmpty(value)) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const webTemplateFile = formatFileName(value!);
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const webTemplateFolder = formatFolderName(value!);
+           
                 const watcher: vscode.FileSystemWatcher =
                     vscode.workspace.createFileSystemWatcher(
                         new vscode.RelativePattern(
                             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                             vscode.workspace.workspaceFolders![0],
-                            `**/web-templates/${webTemplateName}/${webTemplateName}.webtemplate.source.html`
+                            `**/web-templates/${webTemplateFolder}/${webTemplateFile}.webtemplate.source.html`
                         ),
                         false,
                         true,
@@ -34,19 +46,45 @@ export const webTemplate = (context: vscode.ExtensionContext) => {
                     );
 
                 context.subscriptions.push(watcher);
-                vscode.window.withProgress(
-                    {
-                        location: vscode.ProgressLocation.Notification,
-                        cancellable: true,
-                        title: localize("microsoft-powerapps-portals.webExtension.webtemplate.create.message","Creating Web Template"),
-                    },
-                    async () => {
-                        watcher.onDidCreate(async (uri) => {
-                            await vscode.window.showTextDocument(uri);
-                            terminal.dispose();
-                        });
-                    }
-                );
+                const portalDir = `${vscode.workspace.workspaceFolders?.[0].uri.fsPath}`;
+                const yoPath = path.join("node_modules", ".bin", "yo");
+                const yoSubgenerator = "@microsoft/powerpages:webtemplate";
+                const command = `${yoPath} ${yoSubgenerator} "${value}"`;
+
+                vscode.window
+                    .withProgress(
+                        {
+                            location: vscode.ProgressLocation.Notification,
+                            title: "Creating web template...",
+                        },
+                        () => {
+                            return new Promise((resolve, reject) => {
+                                exec(
+                                    command,
+                                    { cwd: portalDir },
+                                    (error, stderr) => {
+                                        if (error) {
+                                            vscode.window.showErrorMessage(
+                                                error.message
+                                            );
+                                            reject(error);
+                                        } else {
+                                            resolve(stderr);
+                                        }
+                                    }
+                                );
+                            });
+                        }
+                    )
+                    .then(() => {
+                        vscode.window.showInformationMessage(
+                            "Web template Created!"
+                        );
+                    });
+
+                watcher.onDidCreate(async (uri) => {
+                    await vscode.window.showTextDocument(uri);
+                });
             }
         });
 };
--- src/client/powerpages/utils/CommonUtils.ts
+++ src/client/powerpages/utils/CommonUtils.ts
@@ -7,3 +7,21 @@ export const isNullOrEmpty = (str: string | undefined): boolean => {
     return !str || str.trim().length === 0;
 };
 
+/**
+ * @example
+ * formatFolderName("My Folder Name"); // "my-folder-name"
+ */
+export const formatFolderName = (name: string) => {
+    return name.replace(/[/ ]/g, "-").toLowerCase();
+};
+
+/**
+ * @example
+ * formatFileName("My File Name"); // "My-File-Name"
+ */
+export const formatFileName = (name: string) => {
+    const words = name.split(/[/ ]/);
+
+    // Uppercase the first letter of each word and join the words back together
+    return words.map((word) => word[0].toUpperCase() + word.slice(1)).join("-");
+};
