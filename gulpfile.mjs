5048501b37aa1b8ccdaf79de6c4424f5782068d7
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-06-15 05:06:59 +0000

    May Refresh

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -368,7 +368,7 @@ async function snapshot() {
 }
 
 const feedName = 'CAP_ISVExp_Tools_Stable';
-const cliVersion = '1.23.3';
+const cliVersion = '1.24.3';
 
 const recompile = gulp.series(
     clean,
