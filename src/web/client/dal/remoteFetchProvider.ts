3be8d9f2cf80546251140f0a5a0609903468ba29
Nidhi Tyagi 🌟🐇🌴❄️ <nityagi@microsoft.com>
2023-06-21 00:13:20 +0530

    minor - fix save

--- src/web/client/dal/remoteFetchProvider.ts
+++ src/web/client/dal/remoteFetchProvider.ts
@@ -210,7 +210,7 @@ async function createContentFiles(
         }
 
         // Create folder paths
-        filePathInPortalFS = filePathInPortalFS ?? `${Constants.PORTALS_URI_SCHEME}:/${portalFolderName}/${subUri}/`;
+        filePathInPortalFS = filePathInPortalFS ?? `${Constants.PORTALS_URI_SCHEME}:/${portalFolderName}/${subUri}`;
         if (exportType && exportType === folderExportType.SubFolders) {
             filePathInPortalFS = `${filePathInPortalFS}/${fileName}/`;
             await portalsFS.createDirectory(
