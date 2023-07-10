f50eac088ac911f9456f667c40fe95dbc898dede
Amit Joshi <amitjoshi@microsoft.com>
2023-07-10 16:58:48 +0530

    fixing constant file name

--- src/common/copilot/Constants.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-
-export const CopilotDisclaimer = `Make sure AI-generated content is accurate and appropriate before using. <a href="https://go.microsoft.com/fwlink/?linkid=2240145">Learn more</a> | <a href="#">View
-terms</a>`;
-export const CodiconStylePathSegments = [ 'src', 'common', 'copilot', 'assets', 'styles', 'codicon.css'];
-export const CopilotStylePathSegments = [ 'src', 'common', 'copilot', 'assets', 'styles', 'copilot.css'];
