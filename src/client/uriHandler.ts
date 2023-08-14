c64295b7b7c305cf190f52fe5553a96d40ce22ed
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-08-14 14:22:28 -0700

    URI handler cleanup and loc

--- src/client/uriHandler.ts
+++ src/client/uriHandler.ts
@@ -4,22 +4,11 @@
  */
 
 import * as vscode from "vscode";
-import { URLSearchParams } from "node:url";
 import { PacWrapper } from "./pac/PacWrapper";
-// import * as fs from "fs-extra";
-// import * as path from "node:path";
 
 export function RegisterUriHandler(pacWrapper: PacWrapper): vscode.Disposable {
     const uriHandler = new UriHandler(pacWrapper);
     return vscode.window.registerUriHandler(uriHandler);
-}
-
-function convertQueryParamsToObject(query: URLSearchParams) : Record<string, string> {
-    const result: Record<string, string> = {};
-    for (const [key, value] of query) {
-        result[key] = value;
-    }
-    return result;
 }
 
 enum UriPath {
@@ -27,7 +16,6 @@ enum UriPath {
 }
 
 class UriHandler implements vscode.UriHandler {
-    private debugOutput = vscode.window.createOutputChannel("Crash Uri Debugging");
     private readonly pacWrapper: PacWrapper;
 
     constructor(pacWrapper: PacWrapper) {
@@ -38,55 +26,36 @@ class UriHandler implements vscode.UriHandler {
     // vscode://<ExtensionName>/<PathArgs>?<QueryArgs>#<FragmentArgs>
     // vscode://microsoft-IsvExpTools.powerplatform-vscode/pcfInit
     public async handleUri(uri: vscode.Uri): Promise<void> {
-
-        const queryArgs = convertQueryParamsToObject(new URLSearchParams(uri.query));
-
-        this.debugOutput.show();
-        this.debugOutput.appendLine(`Recieved URI: ${uri.toString()}`);
-        this.debugOutput.appendLine(`Path: ${uri.path}`);
-        this.debugOutput.appendLine(`Query: ${uri.query}`);
-        this.debugOutput.appendLine(`Fragment: ${uri.fragment}`);
-        this.debugOutput.appendLine(`Authority: ${uri.authority}`);
-        this.debugOutput.appendLine(`Scheme: ${uri.scheme}`);
-        this.debugOutput.appendLine("");
-
         if (uri.path === UriPath.PcfInit) {
-
-            await vscode.commands.executeCommand("workbench.action.openWalkthrough", "pacCLI.pcfWalkthrough#pacCommands", true);
-
-            const openResults = await vscode.window.showOpenDialog({
-                canSelectFiles: false, canSelectFolders: true, canSelectMany: false, openLabel: "Select Folder" }); // TODO - Localize
-
-            if (openResults && openResults.length === 1) {
-                const selectedFolder = openResults[0];
-                // TODO - Check if folder is empty?
-
-                // Option 1 - execute command directly in a terminal, so that user can see the command and its output
-                const terminal = vscode.window.createTerminal({
-                    name: "PAC CLI",
-                    cwd: selectedFolder.fsPath,
-                    isTransient: true,
-                    message: "PCF Init", // TODO - Localize
-                });
-
-                terminal.show();
-                terminal.sendText("pac pcf init");
-
-                // Option 2 - use existing PAC non-interactively.  This will result in not seeing the command output.
-                // const result = await this.pacWrapper.pcfInit(selectedFolder[0].fsPath);
-                // if (result?.Status === "Success") {
-                //     vscode.window.showInformationMessage("PCF project initialized successfully."); // TODO - Localize
-                // } else {
-                //     vscode.window.showErrorMessage("Failed to initialize PCF project."); // TODO - Localize
-                // }
-
-                // Open new workspace folder, if destination was not in existing workspace
-                if(vscode.workspace.getWorkspaceFolder(selectedFolder) === undefined) {
-                    vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders?.length ?? 0, 0, { uri: selectedFolder });
-                }
-
-            }
+            return this.pcfInit();
         }
     }
 
+    async pcfInit(): Promise<void> {
+        const pcfInitLabel = vscode.l10n.t({
+            message: "Select Folder for new PCF Control",
+            comment: ["Do not translate 'PCF' as it is a product name."]
+        });
+
+        const openResults = await vscode.window.showOpenDialog({
+            canSelectFiles: false, canSelectFolders: true, canSelectMany: false, openLabel: pcfInitLabel });
+
+        if (openResults && openResults.length === 1) {
+            const selectedFolder = openResults[0]; // TODO - Consider checking if folder is empty
+
+            const terminal = vscode.window.createTerminal({
+                name: "PAC CLI",
+                cwd: selectedFolder.fsPath,
+                isTransient: true,
+            });
+
+            terminal.show();
+            terminal.sendText("pac pcf init");
+
+            // Open new workspace folder, if destination was not in existing workspace
+            if(vscode.workspace.getWorkspaceFolder(selectedFolder) === undefined) {
+                vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders?.length ?? 0, 0, { uri: selectedFolder });
+            }
+        }
+    }
 }
