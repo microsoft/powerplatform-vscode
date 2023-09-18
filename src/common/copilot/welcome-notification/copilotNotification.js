dfe42c912917e48eaa88ab38333ebe49ab02ef90
amitjoshi <amitjoshi@microsoft.com>
2023-09-18 20:11:32 +0530

    removed commented code

--- src/common/copilot/welcome-notification/copilotNotification.js
+++ src/common/copilot/welcome-notification/copilotNotification.js
@@ -32,13 +32,4 @@
         vscode.postMessage({command:'learnMore'})
     });
 
-    // Handle messages sent from the extension to the webview
-    // window.addEventListener("message", (event) => {
-    //     const message = event.data; // The JSON data our extension sent
-    //     switch (message.type) {
-    //         case:
-    //         break;
-    //     }
-    // });
-
 })();
