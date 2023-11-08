6f15fcad19cbcff6cbee999018a57b4fc80790bd
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-11-08 12:55:32 -0800

    Release Notes and PAC update

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -336,7 +336,7 @@ async function snapshot() {
 }
 
 const feedName = 'CAP_ISVExp_Tools_Stable';
-const cliVersion = '1.28.3';
+const cliVersion = '1.29.2';
 
 const recompile = gulp.series(
     clean,
