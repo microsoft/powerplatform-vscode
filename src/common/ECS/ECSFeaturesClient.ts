bcaef9af8184dd52352a9f4a5b2be06b4cc74f39
Nidhi Tyagi 🌟🐇🌴❄️ <nityagi@microsoft.com>
2023-11-17 10:49:08 +0530

    ignore eslint for now

--- src/common/ECS/ECSFeaturesClient.ts
+++ src/common/ECS/ECSFeaturesClient.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 function createPowerPagesFeature(arg0: { teamName: string; owners: string[]; description: string; createdOn: string; fallback: { enableMultifileVscodeWeb: boolean; }; }): { useFeature: any; feature: any; withFeature: any; } {
     throw new Error('Function not implemented.');
 }
