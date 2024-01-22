556a9c59b46dd4437557be32ec75338f4f21ff34
BidishaMS <97606074+BidishaMS@users.noreply.github.com>
2024-01-22 23:29:04 +0530

    Placeholder for other attributes (#815)
    * Populate data in other fields
    
    * added dummy value

--- src/common/OneDSLoggerTelemetry/oneDSLogger.ts
+++ src/common/OneDSLoggerTelemetry/oneDSLogger.ts
@@ -235,6 +235,28 @@ export class OneDSLogger implements ITelemetryLogger{
                     envelope.data.vscodeExtensionVersion = getExtensionVersion();
                     envelope.data.vscodeVersion = vscode.version;
                     envelope.data.domain = vscode.env.appHost;
+                    // Adding below attributes so they get populated in Geneva. 
+                    // TODO: It needs implementation for populating the actual value
+                    envelope.data.tenantId = "test";
+                    envelope.data.principalObjectId = "test";
+                    envelope.data.puid = "test";
+                    envelope.data.eventSubType = "test";
+                    envelope.data.scenarioId = "test";
+                    envelope.data.eventModifier = "test";
+                    envelope.data.userRegion = "test";
+                    envelope.data.context = "test"; 
+                    envelope.data.timestamp = "test";
+                    envelope.data.country = "test";
+                    envelope.data.userLocale = "test";
+                    envelope.data.userDataBoundary = "test";
+                    envelope.data.appLocale = "test";
+                    envelope.data.userLocale = "test";
+                    envelope.data.webBrowser = "test";
+                    envelope.data.browserVersion = "test";
+                    envelope.data.browserLanguage = "test";
+                    envelope.data.screenResolution = "test";
+                    envelope.data.osName = "test"; 
+                    envelope.data.osVersion = "test";
                     // envelope.data.timestamp = envelope.ext.user.locale;
                     // envelope.data.userLocale = envelope.ext.user.locale;
                     // envelope.data.userTimeZone = envelope.ext.loc.tz;
