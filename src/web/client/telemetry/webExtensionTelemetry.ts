bc8840b60e40f7858c141a5c82ef7471bd27b337
ramukaritik <ramukaritik@microsoft.com>
2024-02-07 16:06:34 +0530

    Fix formatting in webExtensionTelemetry.ts

--- src/web/client/telemetry/webExtensionTelemetry.ts
+++ src/web/client/telemetry/webExtensionTelemetry.ts
@@ -52,7 +52,7 @@ export class WebExtensionTelemetry {
                 siteVisibility: queryParamsMap.get(queryParameters.SITE_VISIBILITY),
                 region: queryParamsMap.get(queryParameters.REGION),
                 geo: queryParamsMap.get(queryParameters.GEO),
-                envId: queryParamsMap.get(queryParameters.ENV_ID),
+                envId: queryParamsMap.get(queryParameters.ENV_ID)
             }
         }
         this._telemetry?.sendTelemetryEvent(telemetryData.eventName, telemetryData.properties);
