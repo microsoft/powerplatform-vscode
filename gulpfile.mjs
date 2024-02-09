086d62cacffe7b49e3bb5cc5a7689776e02eedab
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2024-02-09 11:36:16 -0800

    Auth Profile listing fix

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -336,7 +336,7 @@ async function snapshot() {
 }
 
 const feedName = 'CAP_ISVExp_Tools_Stable';
-const cliVersion = '1.30.6';
+const cliVersion = '1.30.7';
 
 const recompile = gulp.series(
     clean,
