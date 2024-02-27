b8c03b5c0998d35bc49cb92b7ce2b0281860544d
gshivi <123095952+gshivi@users.noreply.github.com>
2024-02-27 13:40:50 +0530

    Init telemetry logger for desktop extension (#826)
    * Init telemetry logger fro desktop extension
    
    * Changes
    
    * Abstract filewatcher into a reusable function
    
    * refactor
    
    * Optimization for activeOrg call: copilot & extension to consume custom event on org change
    
    * Cleaning up - filewatcher from utils
    
    * changes
    
    * Org change failure support
    
    * Removing unused import
    
    * removing reduncdant checks
    
    * Updating comment & line spacing
    
    * Updating comment & line spacing
    
    ---------
    
    Co-authored-by: Shivika Gupta <shivikagupta@microsoft.com>
    Co-authored-by: tyaginidhi <tyaginidhi@users.noreply.github.com>

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -35,6 +35,11 @@ import { bootstrapDiff } from "./power-pages/bootstrapdiff/BootstrapDiff";
 import { CopilotNotificationShown } from "../common/copilot/telemetry/telemetryConstants";
 import { copilotNotificationPanel, disposeNotificationPanel } from "../common/copilot/welcome-notification/CopilotNotificationPanel";
 import { COPILOT_NOTIFICATION_DISABLED } from "../common/copilot/constants";
+import { fetchArtemisResponse } from "../common/ArtemisService";
+import { oneDSLoggerWrapper } from "../common/OneDSLoggerTelemetry/oneDSLoggerWrapper";
+import { OrgChangeNotifier, orgChangeEvent } from "../common/OrgChangeNotifier";
+import { ActiveOrgOutput } from "./pac/PacTypes";
+
 
 let client: LanguageClient;
 let _context: vscode.ExtensionContext;
@@ -61,6 +66,9 @@ export async function activate(
         appInsightsResource.instrumentationKey
     );
     context.subscriptions.push(_telemetry);
+    // Logging telemetry in US cluster for unauthenticated scenario
+    oneDSLoggerWrapper.instantiate("us");
+
     _telemetry.sendTelemetryEvent("Start", {
         "pac.userId": readUserSettings().uniqueId,
     });
@@ -157,12 +165,24 @@ export async function activate(
     const cliContext = new CliAcquisitionContext(_context, _telemetry);
     const cli = new CliAcquisition(cliContext);
     const cliPath = await cli.ensureInstalled();
+    const pacTerminal = new PacTerminal(_context, _telemetry, cliPath);
     _context.subscriptions.push(cli);
-    _context.subscriptions.push(new PacTerminal(_context, _telemetry, cliPath));
+    _context.subscriptions.push(pacTerminal);
     const workspaceFolders =
         vscode.workspace.workspaceFolders?.map(
             (fl) => ({ ...fl, uri: fl.uri.fsPath } as WorkspaceFolder)
         ) || [];
+
+    _context.subscriptions.push(
+        orgChangeEvent(async (orgDetails: ActiveOrgOutput) => {
+            const orgID = orgDetails.OrgId;
+            const artemisResponse = await fetchArtemisResponse(orgID, _telemetry);
+            if (artemisResponse) {
+                const { geoName } = artemisResponse[0];
+                oneDSLoggerWrapper.instantiate(geoName);
+            }
+        })
+    );
 
     // TODO: Handle for VSCode.dev also
     if (workspaceContainsPortalConfigFolder(workspaceFolders)) {
@@ -175,6 +195,9 @@ export async function activate(
         } catch (exception) {
             _telemetry.sendTelemetryException(exception as Error, { eventName: 'VscodeDesktopUsage' });
         }
+        // Init OrgChangeNotifier instance
+        OrgChangeNotifier.createOrgChangeNotifierInstance(pacTerminal.getWrapper());
+
         _telemetry.sendTelemetryEvent("PowerPagesWebsiteYmlExists"); // Capture's PowerPages Users
         vscode.commands.executeCommand('setContext', 'powerpages.websiteYmlExists', true);
         initializeGenerator(_context, cliContext, _telemetry); // Showing the create command only if website.yml exists
