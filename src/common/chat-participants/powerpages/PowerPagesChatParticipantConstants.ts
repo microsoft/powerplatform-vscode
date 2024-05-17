a9a22f408c0bd57205ac78b61890716e144c7c4f
amitjoshi <amitjoshi@microsoft.com>
2024-05-17 15:07:23 +0530

    refactor: Update PowerPagesChatParticipantConstants
    The code changes in the `PowerPagesChatParticipantConstants.ts` file refactor the constants used in the PowerPages chat participant. The changes include:
    - Cleaning up the code formatting
    
    Co-authored-by: amitjoshi <amitjoshi@microsoft.com>

--- src/common/chat-participants/powerpages/PowerPagesChatParticipantConstants.ts
+++ src/common/chat-participants/powerpages/PowerPagesChatParticipantConstants.ts
@@ -4,11 +4,7 @@
  */
 
 export const POWERPAGES_CHAT_PARTICIPANT_ID = 'powerpages';
-
 export const RESPONSE_AWAITED_MSG = 'Working on it...'
-
 export const AUTHENTICATION_FAILED_MSG = 'Authentication failed. Please try again.';
-
 export const COPILOT_NOT_AVAILABLE_MSG = 'Copilot is not available. Please contact your administrator.';
-
 export const PAC_AUTH_NOT_FOUND = 'Active auth profile is not found or has expired. Please try again.';
