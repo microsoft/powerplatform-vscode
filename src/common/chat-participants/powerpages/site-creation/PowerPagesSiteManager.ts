2adb3cda978e4b1bedc77a10183b13bb32225840
amitjoshi <amitjoshi@microsoft.com>
2024-08-28 15:06:24 +0530

    feat: Add nl2SiteJson import and getBatchAndFileUploads function
    This commit adds an import statement for nl2SiteJson in the PowerPagesSiteManager file. It also introduces a new function called getBatchAndFileUploads, which is responsible for generating arrays of site and language operations, component operations, and files to upload. These changes enhance the functionality of the PowerPagesSiteManager class.
    
    Co-authored-by: amitjoshi <amitjoshi@microsoft.com>

--- src/common/chat-participants/powerpages/site-creation/PowerPagesSiteManager.ts
+++ src/common/chat-participants/powerpages/site-creation/PowerPagesSiteManager.ts
@@ -3,6 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
+import { nl2SiteJson } from "./Nl2SiteTemplate";
 import { PowerPagesParsedJson } from "./PowerPagesSiteModel";
 import { reGuidPowerPagesSite } from "./PowerPagesSiteUtils";
 
@@ -29,15 +30,82 @@ import { reGuidPowerPagesSite } from "./PowerPagesSiteUtils";
 
     // Function to fetch and load the template data
     public async loadTemplate(): Promise<void> {
-      const languageCode = 1033 //Only English is supported for now
+      //const languageCode = 1033 //Only English is supported for now
 
-      const ppJsonBlob = (await import(
-        `@ppux-extras/powerpages-assets/site-templates/${this.templateName}/${languageCode}/${this.templateName}.json`
-      )) as unknown;
+      const ppJsonBlob = nl2SiteJson;
 
       this.siteData = reGuidPowerPagesSite(ppJsonBlob as PowerPagesParsedJson);
     }
-    
+
+    function getBatchAndFileUploads(
+        siteData: PowerPagesParsedJson
+      ): [
+        Array<Changeset | DeleteOperation | GetOperation | PatchOperation | PostOperation>,
+        Array<Changeset | DeleteOperation | GetOperation | PatchOperation | PostOperation>,
+        PowerPagesComponent[]
+      ] {
+        // We need site and language to already be created before creating other components in a batch
+        const siteAndLanguages: Array<Changeset | DeleteOperation | GetOperation | PatchOperation | PostOperation> = [];
+        const operations: Array<Changeset | DeleteOperation | GetOperation | PatchOperation | PostOperation> = [];
+
+        // Add site data
+        siteAndLanguages.push({
+          method: 'POST',
+          url: getCDSEntityRequestURLPath({ entityName: entityNames.PowerPagesSites }),
+          headers: {
+            'Content-Type': 'application/json; type=entry',
+          },
+          body: JSON.stringify(siteData.powerpagesite[0]),
+        });
+
+        // Add languages
+        siteData.powerpagesitelanguage.forEach((ppSiteLang) => {
+          const entity = {
+            ...ppSiteLang,
+            [`powerpagesiteid@odata.bind`]: `/${entityNames.PowerPagesSites}(${ppSiteLang.powerpagesiteid!})`,
+          };
+          delete entity.powerpagesiteid;
+          siteAndLanguages.push({
+            method: 'POST',
+            url: getCDSEntityRequestURLPath({ entityName: entityNames.PowerPagesSiteLanguages }),
+            headers: {
+              'Content-Type': 'application/json; type=entry',
+            },
+            body: JSON.stringify(entity),
+          });
+        });
+
+        const filesToUpload: PowerPagesComponent[] = [];
+
+        // Add components
+        siteData.powerpagecomponent.forEach((component) => {
+          if (component.powerpagecomponenttype === PowerPagesComponentType.WebFile && component.filecontent) {
+            filesToUpload.push(component);
+          }
+          const entity = {
+            ...component,
+            [`powerpagesiteid@odata.bind`]: `/${entityNames.PowerPagesSites}(${component.powerpagesiteid!})`,
+            [`powerpagesitelanguageid@odata.bind`]: component.powerpagesitelanguageid
+              ? `/${entityNames.PowerPagesSiteLanguages}(${component.powerpagesitelanguageid})`
+              : null,
+          };
+          delete entity.powerpagesiteid;
+          delete entity.powerpagesitelanguageid;
+          delete entity.filecontent;
+          operations.push({
+            method: 'POST',
+            url: getCDSEntityRequestURLPath({ entityName: entityNames.PowerPagesComponents }),
+            headers: {
+              'Content-Type': 'application/json; type=entry',
+            },
+            body: JSON.stringify(entity),
+          });
+        });
+
+        return [siteAndLanguages, operations, filesToUpload];
+      }
+
+
     // Method to expose site data and actions
     public getSiteDataAndActions(): {
       ppSiteData: PowerPagesParsedJson;
