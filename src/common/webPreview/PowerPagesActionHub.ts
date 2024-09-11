8c0b6be46fdafd413a9ac5f32aac3a1e1dd8b0a0
Ashwani Kumar <ashwanikumar@microsoft.com>
2024-09-11 14:09:55 +0530

    updated preview url

--- src/common/webPreview/PowerPagesActionHub.ts
+++ src/common/webPreview/PowerPagesActionHub.ts
@@ -37,7 +37,7 @@ export class PowerPagesActionHub implements vscode.TreeDataProvider<PowerPagesNo
 
     openSpecificURLWithoutAuth(context: vscode.ExtensionContext): void {
         //const websitePreviewUrl = "https://site-wug4q.powerappsportals.com/"; //public site
-        const websitePreviewUrl = "https://site-nwz9m.powerappsportals.com/"; //private site
+        const websitePreviewUrl = "https://site-tykl8.powerappsportals.com/"; //private site
         const browserLiteExtensionId = 'antfu.browse-lite';
         const browserLiteExtension = vscode.extensions.getExtension(browserLiteExtensionId);
 
