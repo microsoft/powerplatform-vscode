9b03e507feb5b38a42b7cbdecef4fe29cef57cdb
amitjoshi <amitjoshi@microsoft.com>
2024-11-13 19:28:09 +0530

    Refactor CreateSiteHelper to include poplulate record call

--- src/common/chat-participants/powerpages/commands/create-site/CreateSiteHelper.ts
+++ src/common/chat-participants/powerpages/commands/create-site/CreateSiteHelper.ts
@@ -12,11 +12,13 @@ import { oneDSLoggerWrapper } from '../../../../OneDSLoggerTelemetry/oneDSLogger
 import { VSCODE_EXTENSION_NL2PAGE_REQUEST, VSCODE_EXTENSION_NL2SITE_REQUEST } from '../../PowerPagesChatParticipantTelemetryConstants';
 
 export const createSite = async (intelligenceEndpoint: string, intelligenceApiToken: string, userPrompt: string, sessionId: string, stream: vscode.ChatResponseStream, telemetry: ITelemetry, orgId: string, envID: string, userId: string) => {
-    const { siteName, siteDescription } = await fetchSiteAndPageData(intelligenceEndpoint, intelligenceApiToken, userPrompt, sessionId, telemetry, stream, orgId, envID, userId);
+    const { siteName, sitePagesList, sitePages, siteDescription } = await fetchSiteAndPageData(intelligenceEndpoint, intelligenceApiToken, userPrompt, sessionId, telemetry, stream, orgId, envID, userId);
+    const siteManager = await populateRecords(siteName, sitePagesList, sitePages, telemetry);
+    const websiteId = await provisionSite(siteManager);
 
     return {
         siteName,
-        //websiteId,
+        websiteId,
         siteDescription,
     };
 };
@@ -45,4 +47,45 @@ async function fetchSiteAndPageData(intelligenceEndpoint: string, intelligenceAp
     }
 
     return { siteName, sitePagesList, sitePages, siteDescription };
+}
+
+async function populateRecords(siteName: string, sitePagesList: string[], sitePages: any, telemetry: ITelemetry) {
+    // Create a map of sitePagesList and sitePages
+    const sitePagesMap = sitePagesList.reduce((acc: any, pageName: string, index: number) => {
+        acc[pageName] = sitePages[index];
+        return acc;
+    }, {});
+
+    // Initialize PowerPagesSiteManager
+    const siteManager = new PowerPagesSiteManager('BlankTemplate', 'English', telemetry);
+
+    // Load the template
+    await siteManager.loadTemplate();
+    const { actions } = siteManager.getSiteDataAndActions();
+    actions.updateSiteName(siteName);
+
+    const promises = Object.entries(sitePagesMap).map(([pageName, pageContent]) => {
+        if (typeof pageContent === 'object' && pageContent !== null && 'code' in pageContent) {
+            return actions.addOrUpdatePage(pageName, (pageContent as { code: string }).code, pageName === 'Home');
+        } else {
+            throw new Error(`Invalid page content for page: ${pageName}`);
+        }
+    });
+
+    await Promise.all(promises);
+
+    // Save the site
+    await actions.save();
+
+    return siteManager;
+}
+
+async function provisionSite(siteManager: any) {
+    // Provision the site
+    const websiteId = siteManager.getSiteDataAndActions().ppSiteData.powerpagesite[0].powerpagesiteid ?? '';
+    // const portal = await createPortal(siteName, siteUrl, websiteId);
+
+    // GetPortalByIdCall
+
+    return websiteId;
 }
