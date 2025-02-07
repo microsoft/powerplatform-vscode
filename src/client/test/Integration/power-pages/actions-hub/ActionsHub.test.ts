32c13bdea786c5bb8389662bb414c332dd29c569
amitjoshi <amitjoshi@microsoft.com>
2025-02-07 12:01:20 +0530

    Refactor ActionsHub tests to improve logger assertions and add exception handling cases

--- src/client/test/Integration/power-pages/actions-hub/ActionsHub.test.ts
+++ src/client/test/Integration/power-pages/actions-hub/ActionsHub.test.ts
@@ -3,27 +3,27 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { expect } from "chai";
-import * as sinon from "sinon";
-import * as vscode from "vscode";
-import { ActionsHub } from "../../../../power-pages/actions-hub/ActionsHub";
-import { PacTerminal } from "../../../../lib/PacTerminal";
-import { ECSFeaturesClient } from "../../../../../common/ecs-features/ecsFeatureClient";
-import { oneDSLoggerWrapper } from "../../../../../common/OneDSLoggerTelemetry/oneDSLoggerWrapper";
-import { ActionsHubTreeDataProvider } from "../../../../power-pages/actions-hub/ActionsHubTreeDataProvider";
-import { EnableActionsHub } from "../../../../../common/ecs-features/ecsFeatureGates";
+import * as vscode from 'vscode';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ActionsHub } from '../../../../power-pages/actions-hub/ActionsHub';
+import { ECSFeaturesClient } from '../../../../../common/ecs-features/ecsFeatureClient';
+import { EnableActionsHub } from '../../../../../common/ecs-features/ecsFeatureGates';
+import { ActionsHubTreeDataProvider } from '../../../../power-pages/actions-hub/ActionsHubTreeDataProvider';
+import { oneDSLoggerWrapper } from '../../../../../common/OneDSLoggerTelemetry/oneDSLoggerWrapper';
+import { PacTerminal } from '../../../../lib/PacTerminal';
 
 describe('ActionsHub', () => {
     let context: vscode.ExtensionContext;
     let pacTerminal: PacTerminal;
-    let loggerStub: sinon.SinonStub;
+    let loggerStub: sinon.SinonStubbedInstance<any>;
     let ecsFeaturesClientStub: sinon.SinonStub;
     let executeCommandStub: sinon.SinonStub;
 
     beforeEach(() => {
         context = {} as vscode.ExtensionContext;
         pacTerminal = {} as PacTerminal;
-        loggerStub = sinon.stub(oneDSLoggerWrapper, 'getLogger').returns({
+        loggerStub = sinon.createStubInstance(oneDSLoggerWrapper, {
             traceInfo: sinon.stub(),
             traceWarning: sinon.stub(),
             traceError: sinon.stub(),
@@ -61,6 +61,14 @@ describe('ActionsHub', () => {
 
             expect(result).to.be.false;
         });
+
+        it('should handle exceptions and return false', () => {
+            ecsFeaturesClientStub.withArgs(EnableActionsHub).throws(new Error('Test Error'));
+
+            const result = ActionsHub.isEnabled();
+
+            expect(result).to.be.false;
+        });
     });
 
     describe('initialize', () => {
@@ -70,9 +78,11 @@ describe('ActionsHub', () => {
 
             await ActionsHub.initialize(context, pacTerminal);
 
-            expect(loggerStub.calledOnceWithExactly("EnableActionsHub", { isEnabled: 'true' })).to.be.true;
+            expect(loggerStub.traceInfo.calledOnceWithExactly("EnableActionsHub", { isEnabled: 'true' })).to.be.true;
             expect(executeCommandStub.calledOnceWithExactly("setContext", "microsoft.powerplatform.pages.actionsHubEnabled", true)).to.be.true;
             expect(actionsHubTreeDataProviderStub.calledOnceWithExactly(context, pacTerminal)).to.be.true;
+
+            actionsHubTreeDataProviderStub.restore();
         });
 
         it('should log telemetry and set context when ActionsHub is disabled', async () => {
@@ -80,8 +90,38 @@ describe('ActionsHub', () => {
 
             await ActionsHub.initialize(context, pacTerminal);
 
-            expect(loggerStub.calledOnceWithExactly("EnableActionsHub", { isEnabled: 'false' })).to.be.true;
+            expect(loggerStub.traceInfo.calledOnceWithExactly("EnableActionsHub", { isEnabled: 'false' })).to.be.true;
             expect(executeCommandStub.calledOnceWithExactly("setContext", "microsoft.powerplatform.pages.actionsHubEnabled", false)).to.be.true;
+        });
+
+        it('should handle exceptions and log error telemetry', async () => {
+            ecsFeaturesClientStub.withArgs(EnableActionsHub).throws(new Error('Test Error'));
+
+            await ActionsHub.initialize(context, pacTerminal);
+
+            expect(loggerStub.traceError.calledOnce).to.be.true;
+        });
+
+        it('should not initialize ActionsHubTreeDataProvider if ActionsHub is disabled', async () => {
+            ecsFeaturesClientStub.withArgs(EnableActionsHub).returns({ enableActionsHub: false });
+            const actionsHubTreeDataProviderStub = sinon.stub(ActionsHubTreeDataProvider, 'initialize');
+
+            await ActionsHub.initialize(context, pacTerminal);
+
+            expect(actionsHubTreeDataProviderStub.notCalled).to.be.true;
+
+            actionsHubTreeDataProviderStub.restore();
+        });
+
+        it('should initialize ActionsHubTreeDataProvider if ActionsHub is enabled', async () => {
+            ecsFeaturesClientStub.withArgs(EnableActionsHub).returns({ enableActionsHub: true });
+            const actionsHubTreeDataProviderStub = sinon.stub(ActionsHubTreeDataProvider, 'initialize');
+
+            await ActionsHub.initialize(context, pacTerminal);
+
+            expect(actionsHubTreeDataProviderStub.calledOnceWithExactly(context, pacTerminal)).to.be.true;
+
+            actionsHubTreeDataProviderStub.restore();
         });
     });
 });
