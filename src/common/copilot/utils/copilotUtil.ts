cecda7abd24fd567c7363b6e6f2b470142bcd3ba
amitjoshi <amitjoshi@microsoft.com>
2025-02-17 12:40:02 +0530

    Implement createAuthProfileExp function and add tests for authentication profile creation

--- src/common/copilot/utils/copilotUtil.ts
+++ src/common/copilot/utils/copilotUtil.ts
@@ -3,33 +3,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import * as vscode from "vscode";
-import { PacWrapper } from "../../../client/pac/PacWrapper";
 import { ECSFeaturesClient } from "../../ecs-features/ecsFeatureClient";
 import { CopilotDisableList, EnablePowerPagesGitHubCopilot, EnableProDevCopilot } from "../../ecs-features/ecsFeatureGates";
-import { AUTH_CREATE_FAILED, AUTH_CREATE_MESSAGE } from "../constants";
-import { showInputBoxAndGetOrgUrl, showProgressWithNotification } from "../../utilities/Utils";
-import { SUCCESS } from "../../constants";
-
-export async function createAuthProfileExp(pacWrapper: PacWrapper | undefined) {
-    const userOrgUrl = await showInputBoxAndGetOrgUrl();
-    if (!userOrgUrl) {
-        return;
-    }
-
-    if (!pacWrapper) {
-        vscode.window.showErrorMessage(AUTH_CREATE_FAILED);
-        return;
-    }
-
-    const pacAuthCreateOutput = await showProgressWithNotification(vscode.l10n.t(AUTH_CREATE_MESSAGE), async () => { return await pacWrapper?.authCreateNewAuthProfileForOrg(userOrgUrl) });
-    if (pacAuthCreateOutput && pacAuthCreateOutput.Status !== SUCCESS) {
-        vscode.window.showErrorMessage(AUTH_CREATE_FAILED);
-        return;
-    }
-
-    return pacAuthCreateOutput;
-}
 
 export function getDisabledOrgList() {
     const disallowedProDevCopilotOrgs = ECSFeaturesClient.getConfig(CopilotDisableList).disallowedProDevCopilotOrgs;
