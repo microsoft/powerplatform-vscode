467c97e56c3bc70c5c06fef7862951f540eddcaf
Priyanshu Agrawal <priyanshuag@microsoft.com>
2025-02-19 16:19:10 +0530

    [Power Pages][Actions Hub] Add revealInOS command for current active site and update translations (#1131)

--- src/client/power-pages/actions-hub/CurrentSiteContext.ts
+++ src/client/power-pages/actions-hub/CurrentSiteContext.ts
@@ -9,23 +9,28 @@ import { findWebsiteYmlFolder, getWebsiteRecordId } from '../../../common/utilit
 
 interface ICurrentSiteContext {
     currentSiteId: string | null;
+    currentSiteFolderPath: string | null;
 }
 
 class CurrentSiteContext implements ICurrentSiteContext {
     private _currentSiteId: string | null = null;
+    private _currentSiteFolderPath: string | null = null;
     private _disposables: vscode.Disposable[] = [];
-    private readonly _onChanged = new vscode.EventEmitter<string | null>();
+    private readonly _onChanged = new vscode.EventEmitter<ICurrentSiteContext>();
     public readonly onChanged = this._onChanged.event;
 
     constructor() {
-        this._currentSiteId = this.getCurrentSiteId();
+        const { currentSiteId, currentSiteFolderPath } = this.getCurrentSiteInfo();
+        this._currentSiteId = currentSiteId;
+        this._currentSiteFolderPath = currentSiteFolderPath;
 
         this._disposables.push(
             vscode.window.onDidChangeActiveTextEditor(() => {
-                const newSiteId = this.getCurrentSiteId();
-                if (newSiteId !== this._currentSiteId) {
-                    this._currentSiteId = newSiteId;
-                    this._onChanged.fire(newSiteId);
+                const newSiteInfo = this.getCurrentSiteInfo();
+                if (newSiteInfo.currentSiteId !== this._currentSiteId) {
+                    this._currentSiteId = newSiteInfo.currentSiteId;
+                    this._currentSiteFolderPath = newSiteInfo.currentSiteFolderPath;
+                    this._onChanged.fire(newSiteInfo);
                 }
             })
         );
@@ -35,12 +40,16 @@ class CurrentSiteContext implements ICurrentSiteContext {
         return this._currentSiteId;
     }
 
+    public get currentSiteFolderPath(): string | null {
+        return this._currentSiteFolderPath;
+    }
+
     public dispose() {
         this._onChanged.dispose();
         this._disposables.forEach(d => d.dispose());
     }
 
-    private getCurrentSiteId(): string | null {
+    private getCurrentSiteInfo(): ICurrentSiteContext {
         const activeEditor = vscode.window.activeTextEditor;
         if (activeEditor) {
             const filePath = activeEditor.document.uri.fsPath;
@@ -50,7 +59,7 @@ class CurrentSiteContext implements ICurrentSiteContext {
             if (websiteYmlFolder) {
                 const siteId = getWebsiteRecordId(websiteYmlFolder);
                 if (siteId) {
-                    return siteId;
+                    return { currentSiteId: siteId, currentSiteFolderPath: websiteYmlFolder };
                 }
             }
         }
@@ -58,10 +67,10 @@ class CurrentSiteContext implements ICurrentSiteContext {
         // Fallback: check first workspace folder
         const workspaceFolders = vscode.workspace.workspaceFolders;
         if (workspaceFolders && workspaceFolders.length > 0) {
-            return getWebsiteRecordId(workspaceFolders[0].uri.fsPath);
+            return { currentSiteId: getWebsiteRecordId(workspaceFolders[0].uri.fsPath), currentSiteFolderPath: workspaceFolders[0].uri.fsPath };
         }
 
-        return null;
+        return { currentSiteId: null, currentSiteFolderPath: null };
     }
 }
 
