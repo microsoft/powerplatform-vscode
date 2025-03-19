00a95232bdb889ee725d05cb1f47c6c90455442b
Priyanshu Agrawal <priyanshuag@microsoft.com>
2025-03-19 12:23:08 +0530

    Implement CreateDemoSite function to streamline demo site setup in CodeGenPreview

--- src/common/codegen/CodeGenPreview.ts
+++ src/common/codegen/CodeGenPreview.ts
@@ -8,6 +8,7 @@ import { MemFS } from "./FileSystemProvider";
 import { BrowserNPM } from "./BrowserNpm";
 import * as esbuild from 'esbuild-wasm';
 import { resolvePlugin } from "./esbuild";
+import { CreateDemoSite } from "./DemoDataHelper";
 
 export class CodeGenPreview implements vscode.Disposable {
     private static _isInitialized = false;
@@ -24,108 +25,7 @@ export class CodeGenPreview implements vscode.Disposable {
 
         vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.parse('memfs:/'), name: "Codegen Demo" });
 
-        CodeGenPreview._memFs.createDirectory(vscode.Uri.parse(`memfs:/src/`));
-        CodeGenPreview._memFs.createDirectory(vscode.Uri.parse(`memfs:/node_modules/`));
-
-        CodeGenPreview._memFs.writeFile(vscode.Uri.parse(`memfs:/src/index.tsx`), Buffer.from(`
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
-const validationSchema = yup.object({
-    email: yup
-        .string('Enter your email')
-        .email('Enter a valid email!')
-        .required('Email is required'),
-    password: yup
-        .string('Enter your password')
-        .min(8, 'Password should be of minimum 8 characters length')
-        .required('Password is required'),
-});
-
-const WithMaterialUI = () => {
-    const formik = useFormik({
-        initialValues: {
-            email: 'foobar@example.com',
-            password: 'foobar',
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
-    return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <TextField
-                    fullWidth
-                    id="email"
-                    name="email"
-                    label="Email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                />
-                <TextField
-                    fullWidth
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
-                />
-                <Button color="primary" variant="contained" fullWidth type="submit">
-                    Submit
-                </Button>
-            </form>
-        </div>
-    );
-};
-
-ReactDOM.render(<WithMaterialUI />, document.getElementById('root'));
-`.trim()
-        ), { create: true, overwrite: true });
-
-        CodeGenPreview._memFs.writeFile(vscode.Uri.parse(`memfs:/package.json`), Buffer.from(
-            JSON.stringify(
-                {
-                  dependencies: {
-                    '@material-ui/core': '4.11.0',
-                    formik: '2.3.0',
-                    react: '18.2.0',
-                    'react-dom': '18.2.0',
-                    yup: '0.29.3',
-                  },
-                },
-                null,
-                2
-              )
-        ), { create: true, overwrite: true });
-
-        CodeGenPreview._memFs.writeFile(vscode.Uri.parse(`memfs:/esbuild.config.json`), Buffer.from(
-            JSON.stringify(
-                {
-                  entryPoints: ['/src/index'],
-                  outdir: '/dist',
-                  format: 'esm',
-                  write: false,
-                  bundle: true,
-                  plugins: [],
-                },
-                null,
-                2
-              )
-        ), { create: true, overwrite: true });
+        CreateDemoSite(CodeGenPreview._memFs);
     }
 
     public dispose(): void {
