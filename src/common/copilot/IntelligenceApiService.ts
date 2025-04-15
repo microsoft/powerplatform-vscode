dc5cca0536134f01dfb83f29c2f98a4b31270ccb
Amit Joshi <amitjoshi@microsoft.com>
2025-04-15 15:40:35 +0530

    Remove hardcoded test AIB endpoint from sendApiRequest function

--- src/common/copilot/IntelligenceApiService.ts
+++ src/common/copilot/IntelligenceApiService.ts
@@ -29,8 +29,6 @@ export async function sendApiRequest(params: IApiRequestParams) {
         relatedFiles
     } = params;
 
-    const aibEndpointTest = "https://aibuildertextapiservice.wus-il002.gateway.Test.island.powerapps.com/v1.0/ad48417c-a13d-ee11-be6a-00224804265b/appintelligence/chat"
-
     if (!aibEndpoint) {
         return NetworkError;
     }
