c8be02156ac1efefefa705f575b18ab87a8dd454
Rishabh Jain <rishjain@microsoft.com>
2025-09-05 04:23:47 +0530

    Added compare with env to power pages context menu

--- src/client/power-pages/metadata-diff/MetadataDiffDesktop.ts
+++ src/client/power-pages/metadata-diff/MetadataDiffDesktop.ts
@@ -18,9 +18,6 @@ import { ActionsHubTreeDataProvider } from "../actions-hub/ActionsHubTreeDataPro
 export class MetadataDiffDesktop {
     private static _isInitialized = false;
     private static _treeDataProvider: MetadataDiffTreeDataProvider | undefined;
-    // Flag to enable/disable the "Connect & Download" (triggerFlow) command UI
-    // Toggle this to hide/show the option without modifying package.json again.
-    private static _connectAndDownloadEnabled: boolean = false;
 
     static isEnabled(): boolean {
         const enableMetadataDiff = true; //ECSFeaturesClient.getConfig(EnableMetadataDiff).enableMetadataDiff
@@ -61,8 +58,6 @@ export class MetadataDiffDesktop {
             });
 
             vscode.commands.executeCommand("setContext", "microsoft.powerplatform.pages.metadataDiffEnabled", isMetadataDiffEnabled);
-            // Set context for connect & download visibility (independent of overall feature enablement)
-            vscode.commands.executeCommand("setContext", "microsoft.powerplatform.pages.metadataDiff.connectDownloadEnabled", MetadataDiffDesktop._connectAndDownloadEnabled && isMetadataDiffEnabled);
 
             if (!isMetadataDiffEnabled) {
                 return;
