4dbd2b72dd95ba8399ba09fd0366a3dc9ee804d0
Priyanshu Agrawal <priyanshuag@microsoft.com>
2025-12-26 18:39:30 +0530

    Add input validation and error handling (#1402)
    ✨ Introduced user input validation and sanitization across multiple components.
    🔧 Updated error constants to include feedback input validation error.
    🛠️ Refactored relevant functions to utilize the new validation utility.
    - Priyanshu

--- src/common/utilities/Utils.ts
+++ src/common/utilities/Utils.ts
@@ -378,7 +378,7 @@ export function getECSOrgLocationValue(clusterName: string, clusterNumber: strin
 
 //API call to get env list for an org
 export async function getEnvList(endpointStamp: ServiceEndpointCategory | undefined): Promise<IEnvInfo[]> {
-    if(!endpointStamp) {
+    if (!endpointStamp) {
         return [];
     }
     const envInfo: IEnvInfo[] = [];
@@ -452,6 +452,6 @@ export function getBAPEndpoint(serviceEndpointStamp: ServiceEndpointCategory): s
 
 export function getWorkspaceFolders(): WorkspaceFolder[] {
     return vscode.workspace.workspaceFolders?.map(
-                (fl) => ({ ...fl, uri: fl.uri.fsPath } as WorkspaceFolder)
-            ) || [];
+        (fl) => ({ ...fl, uri: fl.uri.fsPath } as WorkspaceFolder)
+    ) || [];
 }
